679436dd970d119d4f9b4dda236a45a0d340e365
erikmanzer21 <erikmanzer21@gmail.com>
2024-10-14 11:32:47 +0000

    status

--- frontend/pages/defects.tsx
+++ frontend/pages/defects.tsx
@@ -218,7 +218,7 @@ export default function Defects() {
                 />
               </Box>
               <Box flexBasis={{ xs: '100%', sm: '48%' }}>
-                <FormControl fullWidth variant="outlined" margin="normal">
+                <FormControl fullWidth required>
                   <InputLabel>Status</InputLabel>
                   <Select
                     value={filter.status}
