05e12989f93b628b91e52d704887714442e851a9
Manz2 <erikmanzer21@gmail.com>
2024-10-30 10:58:38 +0100

    fix

--- gm-backend/src/test/java/com/group/gm/gm_backend/GmBackendApplicationTests.java
+++ gm-backend/src/test/java/com/group/gm/gm_backend/GmBackendApplicationTests.java
@@ -1,13 +1,15 @@
-package com.group.gm.gm_backend;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class GmBackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package com.group.gm.gm_backend;
+ * 
+ * import org.junit.jupiter.api.Test;
+ * import org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest
+ * class GmBackendApplicationTests {
+ * 
+ * @Test
+ * void contextLoads() {
+ * }
+ * 
+ * }
+ */
