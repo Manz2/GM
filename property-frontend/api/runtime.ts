b46ff62d0ce65650ad61a6582d8308221d9173da
Erik Manz <erikmanzer21@gmail.com>
2024-12-02 16:18:21 +0100

    Update runtime.ts

--- property-frontend/api/runtime.ts
+++ property-frontend/api/runtime.ts
@@ -13,7 +13,7 @@
  */
 
 
-export const BASE_PATH = "https://property-backend-563205931618.us-central1.run.app".replace(/\/+$/, "");
+export const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || "http://localhost:8081".replace(/\/+$/, "");
 
 export interface ConfigurationParameters {
     basePath?: string; // override base path
