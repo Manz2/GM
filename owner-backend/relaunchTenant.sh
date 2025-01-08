69fa69af0dee5abb58242069d090b15e18b9d11c
Manz2 <erikmanzer21@gmail.com>
2025-01-08 09:39:03 +0100

    fix: update relaunchTenant.sh parameter count and increment owner-backend version to v1.1.22

--- owner-backend/relaunchTenant.sh
+++ owner-backend/relaunchTenant.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Überprüfen, ob die erforderlichen Parameter übergeben wurden
-if [ "$#" -lt 6 ]; then
+if [ "$#" -lt 5 ]; then
   echo "Usage: $0 <CLUSTER_NAME> <REGION> <PROPERTYVERSION> <MANAGEMENTVERSION> <FINANCEVERSION>"
   exit 1
 fi
