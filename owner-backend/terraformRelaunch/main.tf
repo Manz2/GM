e304c50698dc303eac6023da7211c1fe63c070e0
Manz2 <erikmanzer21@gmail.com>
2025-01-17 12:00:33 +0100

    Update IAM roles for service accounts; add Firestore access conditions and increment version to v1.1.31

--- owner-backend/terraformRelaunch/main.tf
+++ owner-backend/terraformRelaunch/main.tf
@@ -41,7 +41,6 @@ variable "node_machine_type" {
   type        = string
   default     = "e2-medium"
 }
-
 # Service Account erstellen
 resource "google_service_account" "cluster_service_account" {
   account_id   = var.cluster_name
@@ -55,25 +54,32 @@ resource "google_project_iam_member" "service_account_identity" {
   member  = "serviceAccount:${google_service_account.cluster_service_account.email}"
 }
 
-# Rolle: GCS Storage Admin
-resource "google_project_iam_member" "storage_admin" {
-  project = var.project_id
-  role    = "roles/storage.admin"
-  member  = "serviceAccount:${google_service_account.cluster_service_account.email}"
-}
-
-# Rolle: Firestore User
-resource "google_project_iam_member" "datastore_user" {
-  project = var.project_id
-  role    = "roles/datastore.user"
-  member  = "serviceAccount:${google_service_account.cluster_service_account.email}"
-}
-
 # Rolle: Firebase Authentication Admin
 resource "google_project_iam_member" "firebase_auth_admin" {
   project = var.project_id
   role    = "roles/firebaseauth.admin"
   member  = "serviceAccount:${google_service_account.cluster_service_account.email}"
+}
+
+
+# Storage-Bucket-spezifischer Zugriff
+resource "google_storage_bucket_iam_member" "bucket_access" {
+  bucket = var.cluster_name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.cluster_service_account.email}"
+}
+
+# Firestore spezifischer Zugriff
+resource "google_project_iam_member" "firestore_access" {
+  project = var.project_id
+  role    = "roles/datastore.user"
+  member  = "serviceAccount:${google_service_account.cluster_service_account.email}"
+
+  condition {
+    title       = "RestrictFirestoreAccess"
+    description = "Grant access to the specific Firestore databases"
+    expression  = "resource.name.startsWith('projects/${var.project_id}/databases/(default)/documents/${var.cluster_name}') || resource.name.startsWith('projects/${var.project_id}/databases/(default)/documents/${var.cluster_name}parking')"
+  }
 }
 
 resource "google_container_cluster" "primary" {
