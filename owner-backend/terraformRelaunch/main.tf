2fadb18d019d643dbf70ed6de196c4e7bd730b83
Manz2 <erikmanzer21@gmail.com>
2025-01-18 15:07:55 +0100

    Disable external IPs for node configuration and bump owner-backend version to v1.1.38

--- owner-backend/terraformRelaunch/main.tf
+++ owner-backend/terraformRelaunch/main.tf
@@ -112,7 +112,6 @@ resource "google_container_cluster" "primary" {
     node_config {
       machine_type = var.node_machine_type
       disk_size_gb = 50
-      enable_external_ips = false # Externe IPs deaktivieren
 
       oauth_scopes = [
         "https://www.googleapis.com/auth/cloud-platform",
